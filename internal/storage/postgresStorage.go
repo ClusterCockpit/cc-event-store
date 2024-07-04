0b35a4c1170559aa3f6e8737fd7a94acd803c3ac
Jan Eitzinger <jan@moebiusband.org>
2024-07-04 16:17:26 +0200

    Rename storage package

--- internal/storage/postgresStorage.go
+++ internal/storage/postgresStorage.go
@@ -2,7 +2,7 @@
 // All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-package storage2
+package storage
 
 import (
 	"database/sql"
